a4ed91b4fcc5bb6cf700fbc240b6593381f30867
Rob Van Pamel <RVPamel@sag-pc-45.SAG.local>
2015-11-04 19:54:03 +0100

    Express serving a file

--- mean/server.js
+++ mean/server.js
@@ -1,8 +1,8 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send("hello world!");
+	res.sendFile(__dirname +  "/Client/index.html");
 });
 
 var server = app.listen(3000,function(){
